d3a1cf1eb6d3ef1c9d190ff8518d42ef4896405a
Lucas <lucacorva@hotmail.com>
2023-02-02 18:10:22 -0300

    Creando el repositorio

---
+++ script.js
@@ -0,0 +1,20 @@
+/*  ENCRIPTAR
+   1- Crear la Fx de encriptacion
+   2- Conectar la Fx al boton "Encriptar" en HTML
+   3- Capturar el texto escrito en HTML
+   4- Desarrollar la logica de la encripatacion
+   5- Mostrar el msj encriptado*/
+
+   /* Variables: 1- La que toma el texto introducido
+                 2- La que tiene el texto encriptado y lo muestra
+                 3-  */ 
+
+const textArea = document.querySelector(".encriptador__textarea");
+const aEncriptar = textArea.value;
+
+//function encriptar(){
+//   alert("anda");
+//}
+
+//var button = document.querySelector(".encriptador__boton-encriptar");
+//button.onclick = encriptar; 
